80a3799371a382c6b48bc664e551eaf7cab6e4f9
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-12 20:27:30 -0300

    Arquivos iniciais da aplicação

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+
+// Constantes
+const PORT = 8080;
+const HOST = '127.0.0.1';
+
+// App
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('app.bhsul.comentarios esta vivo!\n');
+});
+
+app.post('/',  express.bodyParser(), function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+});
+
+app.listen(PORT);
+console.log('Executando em http://localhost:'  + PORT);
