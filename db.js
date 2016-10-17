ba2fe1eb3e396d84a0eda2aad4748399690f10d2
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2016-10-17 12:39:30 -0200

    continua o erro estranho

--- db.js
+++ db.js
@@ -1,33 +1,33 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
 var sqlpool = mysql.createPool({
-    connectionLimit : 100,
-    host     : 'localhost',
-    user     : 'usuario_simples',
-    password : process.env.SQL_PASSWORD,
-    database : 'bhsul.gestaosite',
-    debug    :  false
+ connectionLimit : 100,
+ host : 'localhost',
+ user : 'usuario_simples',
+ password : process.env.SQL_PASSWORD,
+ database : 'bhsul.gestaosite',
+ debug : false
 });
 
 function exports.handle_database(sql, resultado) {
     pool.getConnection(function(err,connection) {
-        if (err) {
-            console.log('Erro ao conectar ao banco de dados');
+		if (err) {
+			console.log('Erro ao conectar ao banco de dados');
             resultado.json({"code" : 100, "status" : "Erro ao conectar ao banco de dados."});
-            return;
-        }
-        connection.query(sql, function(err,rows) {
-            connection.release();
-            if (!err) {
-                resultado.json(rows);
-            }           
-        });
-        connection.on('error', function(err) {
-	        console.log('Erro ao conectar ao banco de dados');
-            resultado.json({"code" : 100, "status" : "Erro ao conectar ao banco de dados."});
-            return;
-        });
-    });
+			return;
+		}
+		connection.query(sql, function(err,rows) {
+		connection.release();
+		if (!err) {
+			resultado.json(rows);
+		}
+	});
+	connection.on('error', function(err) {
+		console.log('Erro ao conectar ao banco de dados');
+		resultado.json({"code" : 100, "status" : "Erro ao conectar ao banco de dados."});
+		return;
+		});
+	});
 }
 
 exports.consultar = function(res) {
