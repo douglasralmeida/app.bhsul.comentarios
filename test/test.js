fa214ea41c4dc541450fb79d8a7b9527473ed814
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-01-18 17:36:58 -0200

    Erro no package.json

--- test/test.js
+++ test/test.js
@@ -1,10 +1,23 @@
 var chai = require('chai');
-var expect = chai.expect;
-var CartSummary = require('../feedback.js');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
 
-describe('exibirtudo', function() {
-  it('exibirtudo() should return 0 if no items are passed in', function() {
-    var exibirtudo = new exibirtudo([]);
-    expect(exibirtudo()).to.equal(0);
+chai.use(chaiHttp);
+
+describe('Teste', function() {
+  it('Teste basico', function(done) {
+    chai.request(server)
+      .get('/teste')
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
   });
+
+  it('should list a SINGLE blob on /blob/<id> GET');
+  it('should add a SINGLE blob on /blobs POST');
+  it('should update a SINGLE blob on /blob/<id> PUT');
+  it('should delete a SINGLE blob on /blob/<id> DELETE');
 });
+
