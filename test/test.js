072579b192c60be4d545b893bb562b6c5885d79e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2017-01-18 18:47:10 -0200

    Teste unitario - correções

--- test/test.js
+++ test/test.js
@@ -21,14 +21,12 @@ describe('Teste', function() {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
-        res.body[0].should.be.a('array');
         res.body[0].should.have.property('id');
         res.body[0].should.have.property('tipo');
         res.body[0].should.have.property('descricao');
         res.body[0].id.should.equal('1');
         res.body[0].tipo.should.equal('1');
         res.body[0].descricao.should.equal('Um teste');
-        res.body[1].should.be.a('array');
         res.body[1].should.have.property('id');
         res.body[1].should.have.property('tipo');
         res.body[1].should.have.property('descricao');
